dd5d49f0241f56168ae5f27cf83c8da571d63335
kalp12 <kalpmegh2000@gmail.com>
2025-05-17 21:17:50 +0530

    added support for render web

--- app.py
+++ app.py
@@ -201,4 +201,5 @@ def download_file(filename):
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
