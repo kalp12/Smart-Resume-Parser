16ab30e83107b58a103036f2565ba4202292ff67
kalp12 <kalpmegh2000@gmail.com>
2025-05-17 21:02:56 +0530

    added support for render web

--- app.py
+++ app.py
@@ -200,4 +200,5 @@ def download_file(filename):
     return send_from_directory('uploads', filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
